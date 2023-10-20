20910790474aecd273f5e3e437b52d7ebdf87dd7
a2nfinance <levi@a2n.finance>
2023-10-20 17:27:09 +0700

    init admin UI

---
+++ admin/src/controller/store.ts
@@ -0,0 +1,32 @@
+import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit'
+import { persistStore } from 'redux-persist';
+import accountReducer from "./account/accountSlice";
+import projectReducer from "./project/projectSlice";
+
+export function makeStore() {
+    return configureStore({
+        reducer: {
+            account: accountReducer,
+            project: projectReducer
+        },
+        middleware: (getDefaultMiddleware) =>
+            getDefaultMiddleware({
+                serializableCheck: false,
+            }),
+    })
+}
+
+export const store = makeStore()
+
+export type AppState = ReturnType<typeof store.getState>
+
+export type AppDispatch = typeof store.dispatch
+
+export type AppThunk<ReturnType = void> = ThunkAction<
+    ReturnType,
+    AppState,
+    unknown,
+    Action<string>
+    >
+
+export const persistor  = persistStore(store)    
---
+++ admin/src/pages/dao/list.tsx
@@ -0,0 +1,7 @@
+import { DAOList } from "src/components/dao/DAOList";
+
+export default function List() {
+    return (
+        <DAOList />
+    )
+}
