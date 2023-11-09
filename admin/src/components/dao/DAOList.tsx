125a41a0d7d41ae69cc45c7d31cf6dc4185eecba
a2nfinance <levi@a2n.finance>
2023-11-09 16:38:08 +0700

    fix typo

--- admin/src/components/dao/DAOList.tsx
+++ admin/src/components/dao/DAOList.tsx
@@ -1,33 +1,30 @@
-import { List } from "antd";
+import { Card, List } from "antd";
 import { useEffect } from "react";
 import { useAppSelector } from "src/controller/hooks";
 import { getPendingDAOs } from "src/core/dao";
 import { Item } from "./Item";
 
 export const DAOList = () => {
-    const {daos} = useAppSelector(state => state.dao)
+    const { daos } = useAppSelector(state => state.dao)
     useEffect(() => {
         getPendingDAOs()
     }, [])
     return (
-        <List
-            grid={{
-                gutter: 12,
-                column: 3
-            }}
-            size="large"
-            pagination={{
-                onChange: (page) => {
-                    console.log(page);
-                },
-                pageSize: 9,
-                align: "center",
-            }}
-            dataSource={daos}
-            renderItem={(item, index) => (
-                <Item index={index} daoFromDB={item} />
-            )}
-        />
+        <Card title="All pending DAOs" style={{ border: "none" }} headStyle={{ padding: 0, textTransform: "uppercase" }}
+            bodyStyle={{ paddingLeft: 0, paddingRight: 0 }}>
+            <List
+                grid={{
+                    gutter: 12,
+                    column: 3
+                }}
+                size="large"
+                pagination={false}
+                dataSource={daos}
+                renderItem={(item, index) => (
+                    <Item index={index} daoFromDB={item} />
+                )}
+            />
+        </Card>
 
     )
 }
