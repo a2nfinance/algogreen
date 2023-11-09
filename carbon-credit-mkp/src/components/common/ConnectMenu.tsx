ea11e083a7d48c43c2b02681a6e7027ec0632a87
a2nfinance <levi@a2n.finance>
2023-11-09 12:14:44 +0700

    update connect menu

--- carbon-credit-mkp/src/components/common/ConnectMenu.tsx
+++ carbon-credit-mkp/src/components/common/ConnectMenu.tsx
@@ -1,52 +1,43 @@
+import { DisconnectOutlined } from '@ant-design/icons';
 import { useWallet } from '@txnlab/use-wallet'
+import { Button, Card, Col, Image, Row } from 'antd';
+import { GrConnect, GrConnectivity } from "react-icons/gr";
+const { Meta } = Card;
 export const ConnectMenu = () => {
     const { providers, activeAccount } = useWallet()
     return (
-        <div>
+        <Row gutter={8}>
             {providers?.map((provider) => (
-                <div key={provider.metadata.id}>
-                    <h4>
-                        <img
-                            width={30}
-                            height={30}
-                            alt={`${provider.metadata.name} icon`}
-                            src={provider.metadata.icon}
-                        />
-                        {provider.metadata.name} {provider.isActive && '[active]'}
-                    </h4>
+                <Col key={provider.metadata.id} span={8}>
+                    <Card
+                        onClick={provider.connect}
+                        style={{cursor: "pointer"}}
+                        bodyStyle={{padding: 10}}
+                        cover={
+                            <Image
+                                preview={false}
+                                alt={`${provider.metadata.name} icon`}
+                                height={140}
+                                src={provider.metadata.icon}
+                            />
+                        }
 
-                    <div>
-                        <button type="button" onClick={provider.connect} disabled={provider.isConnected}>
-                            Connect
-                        </button>
-                        <button type="button" onClick={provider.disconnect} disabled={!provider.isConnected}>
-                            Disconnect
-                        </button>
-                        <button
-                            type="button"
-                            onClick={provider.setActiveProvider}
-                            disabled={!provider.isConnected || provider.isActive}
-                        >
-                            Set Active
-                        </button>
+                        actions={[
+                            <Button title='Connect' icon={<GrConnect />} type="primary" onClick={provider.connect} disabled={provider.isConnected} />,
+                            <Button title='Disconnect' icon={<DisconnectOutlined />} type="primary" onClick={provider.disconnect} disabled={!provider.isConnected} />,
+                            // <Button title="Set active" type="primary"
+                            //     icon={<GrConnectivity />}
+                            //     onClick={provider.setActiveProvider}
+                            //     disabled={!provider.isConnected || provider.isActive}
+                            // />,
+                        ]}
+                    >
+                        <Meta style={{textAlign: "center"}} title={provider.metadata.name.toUpperCase()} />
+                    </Card>
 
-                        <div>
-                            {provider.isActive && provider.accounts.length && (
-                                <select
-                                    value={activeAccount?.address}
-                                    onChange={(e) => provider.setActiveAccount(e.target.value)}
-                                >
-                                    {provider.accounts.map((account) => (
-                                        <option key={account.address} value={account.address}>
-                                            {account.address}
-                                        </option>
-                                    ))}
-                                </select>
-                            )}
-                        </div>
-                    </div>
-                </div>
+
+                </Col>
             ))}
-        </div>
+        </Row>
     )
 }
